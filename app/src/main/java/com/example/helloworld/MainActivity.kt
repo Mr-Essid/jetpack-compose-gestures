39eb262d4833795df6486775096fb75991391dcb
Amine Essid <mr.essid@mail.com>
2024-12-28 16:07:34 +0100

    requirement: list elements movable

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.helloworld
 
 import android.content.ClipData
-import android.content.ContentValues.TAG
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
@@ -9,35 +8,53 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.collection.IntList
 import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.AnimationSpec
+import androidx.compose.animation.core.FiniteAnimationSpec
+import androidx.compose.animation.core.FloatAnimationSpec
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.draganddrop.dragAndDropSource
 import androidx.compose.foundation.draganddrop.dragAndDropTarget
+import androidx.compose.foundation.gestures.DraggableState
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.anchoredDraggable
 import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.gestures.detectHorizontalDragGestures
 import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.gestures.detectVerticalDragGestures
+import androidx.compose.foundation.gestures.draggable
 import androidx.compose.foundation.gestures.rememberDraggableState
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.ime
+import androidx.compose.foundation.layout.isImeVisible
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.outlined.Menu
+import androidx.compose.material.icons.outlined.MoreVert
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.OutlinedCard
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -62,8 +79,12 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.motionEventSpy
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.example.helloworld.ui.theme.HelloWorldTheme
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.async
@@ -73,7 +94,6 @@ import java.util.UUID
 import kotlin.math.roundToInt
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalFoundationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -82,7 +102,7 @@ class MainActivity : ComponentActivity() {
                 darkTheme = false,
                 dynamicColor = false
             ) {
-                SwipeDemonstration()
+                AddRemoveItemsFromLazyColumnDemonstration()
             }
         }
     }
@@ -310,10 +330,10 @@ fun SwipeDemonstration(modifier: Modifier = Modifier) {
                                                 isWeNeedToRemove = true
                                                 coroutineScope.launch {
                                                     delay(4000)
-                                                    if(isWeNeedToRemove) {
+                                                    if (isWeNeedToRemove) {
                                                         println("element removed!")
                                                         ourMutableListState.remove(it)
-                                                    }else {
+                                                    } else {
                                                         println("element removing canceled")
                                                     }
                                                 }
@@ -341,17 +361,125 @@ fun SwipeDemonstration(modifier: Modifier = Modifier) {
 }
 
 
+
 @Preview(showSystemUi = true)
 @Composable
 private fun LIstDemonstration() {
-   val list_ = List(50) {it} .toMutableStateList()
-    LazyColumn {
-       items(list_)  {
-           ListItem(headlineContent = {
-               Text("Item $it")
-           },
-               modifier = Modifier.clickable { list_.remove(it) }
-           )
-       }
+    val list_ = List(50) {it} .toMutableStateList()
+
+    val ourState = remember {
+        mutableStateOf("")
+    }
+
+    Column(
+        modifier = Modifier.padding(WindowInsets.ime.asPaddingValues())
+    ) {
+        LazyColumn(
+            modifier = Modifier.weight(1f)
+        ) {
+            items(list_) {
+                ListItem(headlineContent = {
+                    Text("Item $it")
+                },
+                    modifier = Modifier.clickable { list_.remove(it) }
+                )
+            }
+        }
+        OutlinedTextField(ourState.value, {ourState.value = it}, modifier = Modifier.fillMaxWidth())
+    }
+}
+
+
+
+/**
+ * @param offset of the y point on the screen
+ * @return index of the element in that y point, null in case no element in that point
+ */
+fun LazyListState.indexOfOffset(offset: Int): Int? {
+    val item = this.layoutInfo.visibleItemsInfo.firstOrNull {offset >= it.offset && offset <= it.offset + it.size}
+    return item?.index
+}
+
+
+
+@OptIn(ExperimentalFoundationApi::class)
+@Preview(showSystemUi = true)
+@Composable
+private fun AddRemoveItemsFromLazyColumnDemonstration() {
+    val ourLazyListState = rememberLazyListState()
+    val coroutineScope = rememberCoroutineScope()
+    Scaffold { paddingValues ->
+        Column(
+            modifier = Modifier
+                .padding(paddingValues)
+                .padding(horizontal = 4.dp)
+        ) {
+            Text(
+                "Add And Remove Demonstration", modifier = Modifier.padding(
+                    vertical = 8.dp
+                )
+            )
+            val ourMutableListState = List(40) { it }.toMutableStateList()
+            LazyColumn(
+                state = ourLazyListState,
+                modifier = Modifier
+                    .dragAndDropTarget(shouldStartDragAndDrop = {true}, target = object : DragAndDropTarget{
+                        override fun onMoved(event: DragAndDropEvent) {
+                            super.onMoved(event)
+                            val y = event.toAndroidDragEvent().y
+                            if(ourLazyListState.firstVisibleItemIndex > 0 && y < 200f) // just px value
+                                coroutineScope.launch {
+                                    ourLazyListState.animateScrollToItem(ourLazyListState.firstVisibleItemIndex - 1)
+                                }
+                        }
+                        override fun onDrop(event: DragAndDropEvent): Boolean {
+                            val ourData = event.toAndroidDragEvent().clipData.getItemAt(0).text.toString().toIntOrNull()
+                            val y = event.toAndroidDragEvent().y
+                            println("y is $y")
+                            println("size is ${ourLazyListState.layoutInfo.visibleItemsInfo[0].size}")
+                            val index = ourLazyListState.indexOfOffset(
+                                y.roundToInt()
+                            )
+                            ourData?.let { data ->
+                                index?.let {
+                                    ourMutableListState.add(if(it > 0 )it - 1 else 0,data)
+                                }
+                            }
+                            return true
+                        }
+                    })
+            ) {
+                items(ourMutableListState,
+                    key = { UUID.randomUUID().toString() }
+                ) {
+                    Box(
+                        contentAlignment = Alignment.CenterStart,
+                        modifier = Modifier.dragAndDropSource {
+                           detectTapGestures(onLongPress =  { offset ->
+                               startTransfer(
+                                  DragAndDropTransferData(ClipData.newPlainText("label",it.toString()))
+                               )
+                               ourMutableListState.remove(it)
+                           })
+                        }.animateItemPlacement()
+//                         dp = pic / density
+                    ) {
+                        ListItem(
+                            modifier = Modifier
+                                .padding(vertical = 2.dp)
+                                .clip(RoundedCornerShape(4.dp)),
+                            colors = ListItemDefaults.colors(containerColor = Color(0xffeeeeee)),
+                            headlineContent = {
+                                Text("Item $it")
+                            },
+                            leadingContent = {
+                               Icon(Icons.Outlined.MoreVert, contentDescription = null)
+                            }
+
+                        )
+                    }
+                }
+            }
+        }
     }
 }
